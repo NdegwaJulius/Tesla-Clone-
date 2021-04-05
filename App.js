893ff60c029049839cd173c3385c8ff250af53c0
NdegwaJM <juliusndegwa2@gmail.com>
2021-04-05 09:35:44 +0300

    create  a separate component for CarItem

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import CarItem from './components/CarItem';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       
-      <View style={styles.carContainer}>
-        <ImageBackground 
-        source ={require('./assets/images/ModelS.jpeg')}
-        style={styles.image}
-        /> 
-        
-        <View style={styles.titles}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subtitle}>Starting at $69,420</Text>
-        </View>
-
-      </View>
+      <CarItem />
 
       <StatusBar style="auto" />
     </View>
@@ -29,35 +20,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  carContainer: {
-    width: '100%',
-    height: '100%'
-
-  },
-  titles:{
-    marginTop:'30%',
-    width:'100%',
-    alignItems: 'center'
-
-  },
-  title:{
-    fontSize:40,
-    fontWeight:'500'
-
-  },
-  subtitle:{
-    fontSize: 16,
-    color:'#5c5e62'
-
-  },
-  image:{
-    width:'100%',
-    height:'100%',
-    resizeMode:'cover',
-    position:'absolute',
-
+    justifyContent: 'center'
   }
 
 });
